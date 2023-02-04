8b198625357eb2dcb0cece1270de583a8618dd23
Dhruwang <dhruwangjariwala18@gmail.com>
2023-02-04 21:04:16 +0530

    navbar added

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import logo from "../images/logo.png"
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='navbarInner'>
+            <div className='navbarInnerLeft'>
+                <img id='logo' src={logo}></img>
+            </div>
+            <div className='navbarInnerRight'>
+                <ul className='navbarList'>
+                    <li>Home</li>
+                    <li>Stories</li>
+                    <li>Investors</li>
+                    <li>About</li>
+                    <li>Login/Signup</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
