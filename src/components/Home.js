a8f8531328e47009a2a2944cb6c723868d6d36fa
Dhruwang <dhruwangjariwala18@gmail.com>
2023-02-05 10:19:22 +0530

    background of homePage done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import vector from '../images/vector.png' 
 
 export default function Home() {
   return (
     <div className='home'>
-        
+        <div className='vectorBackground'>
+            <img src={vector}></img>
+        </div>
     </div>
   )
 }
