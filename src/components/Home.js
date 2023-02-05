1401af0ad437e59e9dfbc9726138b2917e5a5924
Dhruwang <dhruwangjariwala18@gmail.com>
2023-02-05 23:12:20 +0530

    added homepage buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import vector from '../images/vector.png' 
+import vector from '../images/vector.png'
 
 export default function Home() {
-  return (
-    <div className='home'>
-        <div className='vectorBackground'>
-            <div className='homeContent'>
-                <h1>"Stories That Inspires"</h1>
-                <p className='text-secondary'>We tell the stories of startups that are making a difference</p>
-                <button className='btn'>Learn More</button>
+    return (
+        <div className='home'>
+            <div className='vectorBackground'>
+                <div className='homeContent'>
+                    <h1>"Stories That Inspires"</h1>
+                    <p className='text-secondary'>We tell the stories of startups that are making a difference</p>
+                    <div className='btn-container'>
+                        <button className='btn'>Learn More</button>
+                        <button className='btn-inverted'>Stories</button>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
