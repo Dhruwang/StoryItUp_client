006d508dc7b41f65a83947b1bc313b97bafe84d2
Dhruwang <dhruwangjariwala18@gmail.com>
2023-02-06 11:31:25 +0530

    completed homepage design

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,16 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='home'>
-        
+      <div className='vectorBackground'>
+        <div className='homeContent'>
+          <h1>"Stories That Inspires"</h1>
+          <p className='text-secondary'>We tell the stories of Startups that are making a difference</p>
+          <div className='btnContainer'>
+            <button className='btn'>Publish</button>
+            <button className='btn-inverted'>Stories</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
