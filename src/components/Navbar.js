e7802c2e0c885b55241f3bfeaa97ff4f56a06acd
Dhruwang <dhruwangjariwala18@gmail.com>
2023-02-06 12:05:27 +0530

    homePage completed along with responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,23 +2,34 @@ import React from 'react'
 import logo from "../images/logo.png"
 
 export default function Navbar() {
-  return (
-    <div className='navbar'>
-        <div className='navbarInner'>
-            <div className='navbarInnerLeft'>
-                <img id='logo' src={logo}></img>
-            </div>
-            <div className='navbarInnerRight'>
-                <ul className='navbarList'>
-                    <li>Home</li>
-                    <li>Stories</li>
-                    <li>Investors</li>
-                    <li>About</li>
-                    <li>Login/Signup</li>
-                </ul>
-                <i class="bi bi-list-nested hamburgerIcon"></i>
+    const handleResponsiveNavbar = ()=>{
+        const sideNavbar = document.getElementById("sideNavbar");
+        if(sideNavbar.style.left==="100vw"){
+            sideNavbar.style.left="0vw"
+        }
+        else{
+            sideNavbar.style.left="100vw" 
+        }
+    }
+    return (
+        <div className='navbar'>
+            <div className='navbarInner'>
+                <div className='navbarInnerLeft'>
+                    <img id='logo' src={logo}></img>
+                </div>
+                <div className='navbarInnerRight'>
+                    <ul className='navbarList'>
+                        <li>Home</li>
+                        <li>Stories</li>
+                        <li>Investors</li>
+                        <li>About</li>
+                        <li>Login/Signup</li>
+                    </ul>
+                    <button className='navbarBtn' onClick={handleResponsiveNavbar}>
+                        <i class="bi bi-list-nested hamburgerIcon"></i>
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
--- src/components/SideNavbar.js
+++ src/components/SideNavbar.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
 export default function SideNavbar() {
+  const handleResponsiveNavbar = ()=>{
+    const sideNavbar = document.getElementById("sideNavbar");
+    if(sideNavbar.style.left==="100vw"){
+        sideNavbar.style.left="0vw"
+    }
+    else{
+        sideNavbar.style.left="100vw" 
+    }
+}
   return (
-    <div className='sideNavbar'>
+    <div className='sideNavbar' id='sideNavbar'>
+      <button className='navbarBtn'onClick={handleResponsiveNavbar}>
+        <i class="bi bi-x cross"></i>
+      </button>
       <div className='sideNavbarInner'>
         <ul>
           <li>Home</li>
