6807a29a1145a8d30bb9af13832b682670258bf3
Dhruwang <dhruwangjariwala18@gmail.com>
2023-02-10 11:02:30 +0530

    implemented search

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -6,7 +6,8 @@ import StoryCard from './StoryCard'
 export default function Stories(props) {
 
     const [stories, setstories] = useState()
-    const host = "https://storyitupbackend.onrender.com"
+    const host = "https://storyitupbackend.onrender.com" 
+    // const host = "http://localhost:8000" 
 
     const fetchStories = async () => {
         const response = await fetch(`${host}/story/fetchStories`, {
@@ -18,6 +19,18 @@ export default function Stories(props) {
         const json = await response.json()
         setstories(json)
 
+    }
+    async function handleSearch (){
+        const response = await fetch(`${host}/story/searchStory`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body:JSON.stringify({ search: document.getElementById("search").value })
+        });
+        const json = await response.json()
+        setstories(json)
+        
     }
 
     useEffect(() => {
@@ -37,19 +50,12 @@ export default function Stories(props) {
                         <h4>ALL STORIES</h4>
                         <div className='searchAndFilters'>
                             <div className='storySearch'>
-                                <input type="text" placeholder='Search Stories'></input>
+                                <input type="text" placeholder='Search Stories' id='search' name="search" onChange={handleSearch}></input>
                                 <button className='btn'>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                                     </svg>
                                 </button>
-                            </div>
-                            <div className='categories'>
-                                <select name="role" id="role" className=' categoryDropdown'>
-                                    <option value="reader">All</option>
-                                    <option value="startupOwner">Education</option>
-                                    <option value="investor">Finance</option>
-                                </select>
                             </div>
                         </div>
 
